23e8f9ddee1be974b85120ffc9b3785cafceca17
KhalezAlex <klozevitz@yandex.ru>
2024-03-02 12:50:01 +0300

    Тестовый проект компании Лайт Диджитал

---
+++ src/main/java/org/klozevitz/lightdigital/LightDigitalApplication.java
@@ -0,0 +1,13 @@
+package org.klozevitz.lightdigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightDigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/klozevitz/lightdigital/LightDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.klozevitz.lightdigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
